c494ff787eb07c8cba51d34d6efa99f445a3f56d
JoseWald <rajaonarivonyjosewald@gmail.com>
2024-11-21 08:30:11 +0300

    test 1 finished with succes

--- chatbot.py
+++ chatbot.py
@@ -6,7 +6,7 @@ import nltk
 
 from nltk.stem import WordNetLemmatizer
 
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 
 lemmatizer = WordNetLemmatizer()
 
@@ -18,7 +18,7 @@ model = load_model("chatbot.h5")
 
 def clean_up_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
-    sentence_words = [lemmatizer.lemmatizr(word) for word in sentence_words]
+    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
     return sentence_words
 
 def bag_of_words(sentence):
@@ -32,22 +32,23 @@ def bag_of_words(sentence):
 
 def predict_class(sentence):
     bow = bag_of_words(sentence)
-    res = model.predict(np.array(bow))[0]
+    res = model.predict(np.array([bow]))[0]
+
 
     ERROR_THRESHOLD = 0.25
     results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
     results.sort(key = lambda x:x[1] , reverse=True)
     return_list= []
     for r in results:
-        return_list.append({'intent':classes[r[0]] , 'probability':str[r[1]]})
+        return_list.append({'intent':classes[r[0]] , 'probability':str([r[1]])})
     return return_list
 
 def get_response(intents_list , intents_json):
     list_of_intents = intents_json['intents']
-    tag = intents_list[0]['intents']
+    tag = intents_list[0]['intent']
 
     for i in list_of_intents:
-        if i['tags'] == tag:
+        if i['tag'] == tag:
             result = random.choice(i['responses'])
             break
     return result
